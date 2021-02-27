4173cf372f8afe1236e5666872564dc8a4569339
Sravani Das Gupta <sravani@Parthas-MacBook-Air.local>
2021-02-26 18:06:49 -0800

    Portfolio Version 1.0

---
+++ assingments/week6/pyramidbuilder.js
@@ -0,0 +1,63 @@
+/*
+
+
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+
+*/
+
+
+console.log("*");
+console.log("**");
+console.log("***");
+console.log("****");
+console.log("*****");
+console.log("****");
+console.log("***");
+console.log("**");
+console.log("*");
+
+
+console.log("*");
+console.log("**");
+console.log("***");
+console.log("****");
+console.log("*****");
+console.log("****");
+console.log("***");
+console.log("**");
+console.log("*");
+
+var currentColumn = "*";
+
+for (var i = 0; 1 <7;  i++ ) {
+     console.log("*");
+
+     currentColumn = currentColumn + "*";
+}
+
+
+
+
+
+
+
+
+
+//string to Arrays
+
+var myString = "Sravani";
+
+var myNewArray = myString.split(" ");
+
+console.log(myNewArray);
+
+myNewArray.pop();
+
